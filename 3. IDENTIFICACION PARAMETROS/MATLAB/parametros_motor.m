f5298d0dae0357cad55d2a4c7ce03180153722a5
dbsrunsaac <dbsrunsaac@unsaac.edu.pe>
2024-10-17 04:34:46 -0500

    Funcion de transferencia y Toolbox de MATLAB

---
+++ 3. IDENTIFICACION PARAMETROS/MATLAB/parametros_motor.m
@@ -0,0 +1,23 @@
+Kt = 0.0436;
+Kb = 0.0436;
+Ra = 17;
+L = 9.35e-3;
+J = 1.6e-6;
+b = 1.4e-6;
+
+numerador = [Kt];
+denominador = [J*L (J*Ra + L*b) (b*Ra + Kb*Kt)];
+
+H = tf(numerador, denominador)
+
+t = linspace(0,10,1000);    
+u = ones(size(t));
+
+y1 = lsim(H,u,t);
+figure;
+plot(t,y1,'b',t,u,'r--')
+xlabel('Tiempo (s)')
+ylabel('Amplitud')
+title('Respuesta transitoria')
+legend('Salida del sistema','Escalón unitario')
+grid on
